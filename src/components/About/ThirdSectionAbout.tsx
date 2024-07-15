2b297f184d8942279bf985757f0d020eaeacef19
Joseph-985 <netochukwu06@gmail.com>
2024-07-15 21:59:02 +0100

    fix:ui fix

--- src/components/About/ThirdSectionAbout.tsx
+++ src/components/About/ThirdSectionAbout.tsx
@@ -4,8 +4,8 @@ const ThirdSectionAbout = () => {
   return (
     <>
       <div className="w-full px-[16px] lg:px-[64px] mt-5 lg:my-14">
-        <div className="grid grid-row-2 lg:grid-cols-4">
-          <div className="col-span-1 font-bold text-[25px] leading-[24px] text-[#E15B3B] lg:text-[35px] lg:leading-6">
+        <div className="grid grid-row-2 lg:grid-cols-4 items-start">
+          <div className="col-span-1 mt-6 font-bold text-[25px] leading-[24px] text-[#E15B3B] lg:text-[35px] lg:leading-6">
             About us
           </div>
           <div className="col-span-1 mt-5 lg:col-span-3 font-normal text-base leading-[22px] lg:text-2xl lg:leading-[30px] pr-5">
